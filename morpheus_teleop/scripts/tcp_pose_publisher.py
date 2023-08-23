1bf4013d41a6239b00a97a73a08036812b6d56c0
Brian Huo <huoshengyu@berkeley.edu>
2023-08-23 11:36:40 -0700

    Ian's fixes, pose publisher

---
+++ morpheus_teleop/scripts/tcp_pose_publisher.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+
+def transform_callback(tf_buffer):
+    try:
+        # Lookup the transform from source_frame to target_frame
+        transform = tf_buffer.lookup_transform("base_link", "tcp_link", rospy.Time(0), rospy.Duration(1.0))
+        
+        # Create a Pose message
+        pose_msg = geometry_msgs.msg.Pose()
+        pose_msg.position.x = transform.transform.translation.x
+        pose_msg.position.y = transform.transform.translation.y
+        pose_msg.position.z = transform.transform.translation.z
+        pose_msg.orientation.x = transform.transform.rotation.x
+        pose_msg.orientation.y = transform.transform.rotation.y
+        pose_msg.orientation.z = transform.transform.rotation.z
+        pose_msg.orientation.w = transform.transform.rotation.w
+        
+        # Publish the Pose message
+        pose_pub.publish(pose_msg)
+        
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        rospy.logwarn("Transform lookup failed!")
+
+if __name__ == '__main__':
+    rospy.init_node('transform_to_pose_node')
+    
+    tf_buffer = tf2_ros.Buffer()
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+    
+    pose_pub = rospy.Publisher('/tcp_pose', geometry_msgs.msg.Pose, queue_size=10)
+    
+    rate = rospy.Rate(10)  # 10 Hz
+    
+    while not rospy.is_shutdown():
+        transform_callback(tf_buffer)
+        rate.sleep()
--- morpheus_teleop/scripts/xbox360_twist.py
+++ morpheus_teleop/scripts/xbox360_twist.py
@@ -6,56 +6,72 @@ import sensor_msgs.msg
 import control_msgs.msg
 from threading import Lock
 from copy import deepcopy
+from collections import deque
 
 from robotiq_2f_gripper_control.msg import Robotiq2FGripper_robot_output
-from onrobot_rg2ft_msgs.msg import RG2FTCommand
 
 class TeleopTwistJoy():
     def __init__(self):
-        self.twist_pub = rospy.Publisher('/joy/twist', geometry_msgs.msg.TwistStamped, queue_size=1)
-        self.gripper_pub = rospy.Publisher('/onrobot_rg2ft/command', RG2FTCommand, queue_size=1)
+        twist_topic = rospy.get_param("~twist_topic", "/joy/twist")
+
+        self.twist_pub = rospy.Publisher(twist_topic, geometry_msgs.msg.Twist, queue_size=1)
+        self.gripper_pub = rospy.Publisher('/robotiq_2f_85_gripper/control', Robotiq2FGripper_robot_output, queue_size=1)
         self.joy_sub = rospy.Subscriber("/joy", sensor_msgs.msg.Joy, self.callback)
         
         self.joy_msg = None
         self.joy_msg_mutex = Lock()
+
+        self.twist_filter_size = 5  # Adjust this value for the moving average window size
+        self.linear_x_buffer = deque(maxlen=self.twist_filter_size)
+        self.linear_y_buffer = deque(maxlen=self.twist_filter_size)
+        self.linear_z_buffer = deque(maxlen=self.twist_filter_size)
+        self.angular_x_buffer = deque(maxlen=self.twist_filter_size)
+        self.angular_y_buffer = deque(maxlen=self.twist_filter_size)
+        self.angular_z_buffer = deque(maxlen=self.twist_filter_size)
 
     def publish(self):
         self.joy_msg_mutex.acquire()
         msg = deepcopy(self.joy_msg)
         self.joy_msg_mutex.release()
 
-        if msg != None:
-            twist = geometry_msgs.msg.TwistStamped()
-            twist.header.stamp = rospy.Time.now()
-            twist.header.frame_id = "world"
-
+        if msg is not None:
             axes = list(self.joy_msg.axes)
             buttons = list(self.joy_msg.buttons)
-            
+
             for i in range(len(axes)):
                 if abs(axes[i]) < 0.15:
                     axes[i] = 0
 
-            linear_scale = 1
-            angular_scale = 1
+            linear_scale = 0.05
+            angular_scale = 0.05
 
-            twist.twist.linear.x = (axes[1])  * linear_scale
-            twist.twist.linear.y = (axes[0])  * linear_scale
-            twist.twist.linear.z = ((1-(axes[5]+1)/2)-(1-(axes[2]+1)/2))  * linear_scale
-            twist.twist.angular.x = (-axes[3])   * angular_scale
-            twist.twist.angular.y = (axes[4])  * angular_scale
-            twist.twist.angular.z = (buttons[2] - buttons[3])  * angular_scale
+            self.linear_x_buffer.append(axes[1] * linear_scale)
+            self.linear_y_buffer.append(axes[0] * linear_scale)
+            self.linear_z_buffer.append(((1 - (axes[5] + 1) / 2) - (1 - (axes[2] + 1) / 2)) * linear_scale)
+            self.angular_x_buffer.append(-axes[3] * angular_scale)
+            self.angular_y_buffer.append(axes[4] * angular_scale)
+            self.angular_z_buffer.append((buttons[2] - buttons[3]) * angular_scale)
 
+            twist = geometry_msgs.msg.Twist()
+            twist.linear.x = sum(self.linear_x_buffer) / len(self.linear_x_buffer)
+            twist.linear.y = sum(self.linear_y_buffer) / len(self.linear_y_buffer)
+            twist.linear.z = sum(self.linear_z_buffer) / len(self.linear_z_buffer)
+            twist.angular.x = sum(self.angular_x_buffer) / len(self.angular_x_buffer)
+            twist.angular.y = sum(self.angular_y_buffer) / len(self.angular_y_buffer)
+            twist.angular.z = sum(self.angular_z_buffer) / len(self.angular_z_buffer)
 
-            command = RG2FTCommand()
-            command.TargetForce = 30
-            command.Control = 0x0001
-            
+            command = Robotiq2FGripper_robot_output()
+            command.rACT = 0x1
+            command.rGTO = 0x1 # go to position
+            command.rATR = 0x0 # No emergency release
+            command.rSP = 128 # speed
+            command.rPR = 0x0 # position
+            command.rFR = 30 # effort
             if buttons[0] == 1:
-                command.TargetWidth = 0 # position
+                command.rPR = 230 # position
                 self.gripper_pub.publish(command)
             elif buttons[1] == 1:
-                command.TargetWidth = 1000 # position
+                command.rPR = 0 # position
                 self.gripper_pub.publish(command)
 
             self.twist_pub.publish(twist)
@@ -64,15 +80,13 @@ class TeleopTwistJoy():
         self.joy_msg_mutex.acquire()
         self.joy_msg = msg
         self.joy_msg_mutex.release()
-        
-
 
 if __name__ == '__main__':
     rospy.init_node('xbox360_twist')
 
     teleop_twist_joy = TeleopTwistJoy()
 
-    rate = rospy.Rate(60) # 10hz
+    rate = rospy.Rate(60)  # 10hz
     while not rospy.is_shutdown():
         teleop_twist_joy.publish()
         rate.sleep()
