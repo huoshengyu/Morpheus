fe0b750b44c76bb52f4ae53b6fcd407800cec9a3
Brian Huo <huoshengyu@berkeley.edu>
2024-02-06 22:53:05 -0800

    Fixed callbacks, added distance-recording infrastructure (incomplete)

--- morpheus_data/src/data.cpp
+++ morpheus_data/src/data.cpp
@@ -78,11 +78,19 @@ class DataNode
         std::stringstream g_next_line;
 
         ros::Subscriber g_contactmap_subscriber;
-        morpheus_msgs::ContactMap latest_contactmap;
-        bool received_contactmap;
+        inline static morpheus_msgs::ContactMap g_latest_contactmap;
+        inline static bool received_contactmap;
+
+        std::vector<std::string> g_robot_links;
+        std::vector<std::string> g_obstacle_links;
+        std::vector<std::string> g_column_label_vector;
 
         DataNode(int argc, char** argv)
         {
+            // Select links to track
+            g_robot_links = A_BOT_LINK_VECTOR;
+            g_obstacle_links = OBSTACLE_VECTOR;
+            
             // Initialize ROS node
             ros::init(argc, argv, "data");
             ros::NodeHandle nh;
@@ -107,7 +115,7 @@ class DataNode
             g_planning_scene_monitor->startWorldGeometryMonitor();
             g_planning_scene_monitor->startStateMonitor("/joint_states");
 
-            g_contactmap_subscriber = nh.subscribe("/collision/contactmap", 1, emptyCallback);
+            g_contactmap_subscriber = nh.subscribe("/collision/contactmap", 1, contactMapCallback);
 
             // Write a header for the file
             // TODO: make header strings dynamic, based on rostopics or arguments
@@ -136,23 +144,28 @@ class DataNode
             g_file << header.str() << std::endl;
 
             // Add column labels to the file
-            std::stringstream column_labels;
-            std::vector<std::string> column_label_vector;
-            column_label_vector.push_back("Time");
-            column_label_vector.push_back("Distance");
-            
-            std::map<std::string, double> current_state_values = g_planning_scene_monitor->getStateMonitor->getCurrentStateValues();
+            std::stringstream column_label_ss;
+            g_column_label_vector.push_back("Time");
+            g_column_label_vector.push_back("Distance");
+            // Add robot links to labels, for tracking positions
+            std::map<std::string, double> current_state_values = getCurrentStateValues();
             for (auto const& key_value : current_state_values)
             {
-                column_label_vector.push_back(key_value.first);
+                g_column_label_vector.push_back(key_value.first);
+            }
+            // Add robot links to labels, for tracking distances
+            for (std::string link : g_robot_links)
+            {
+                g_column_label_vector.push_back(link);
             }
 
-            for (std::string label : column_label_vector)
+            // Add all labels to stringstream
+            for (std::string label : g_column_label_vector)
             {
-                column_labels << label << ", ";
+                column_label_ss << label << ", ";
             }
-            column_labels << std::endl;
-            g_file << column_labels.str();
+            column_label_ss << std::endl;
+            g_file << column_label_ss.str();
 
             received_contactmap = false;
         }
@@ -195,14 +208,41 @@ class DataNode
             std::chrono::duration<double, std::milli> time_elapsed = t2 - t1;
             g_next_line << time_elapsed.count() << ", ";
 
-            // Get link positions
-            // TODO: Listener should request from planning scene monitor
-            // Should include a column for every link
-            std::map<std::string, double> current_state_values = g_planning_scene_monitor->getStateMonitor->getCurrentStateValues();
+            // Get joint angles
+            // Should include a column for every robot joint
+            std::map<std::string, double> current_state_values = getCurrentStateValues();
+            for (auto const& key_value : current_state_values)
+            {
+                g_next_line << key_value.second << ", ";
+            }
 
             // Get contact map
             // TODO: Listener should request from collision/contactmap topic
-            // Should include a column for every link pair
+            // Should include a column for every robot link
+            std::vector<moveit_msgs::ContactInformation> contact_info_vector = g_latest_contactmap.values;
+            std::vector<double> sorted_distances;
+            for (moveit_msgs::ContactInformation contact_info : contact_info_vector)
+            {
+                std::string link_1 = contact_info.contact_body_1;
+                std::string link_2 = contact_info.contact_body_2;
+                
+                auto it = std::find(g_robot_links.begin(), g_robot_links.end(), link_1);
+                if (it != g_robot_links.end())
+                {
+                    int index = it - g_robot_links.begin();
+                    sorted_distances[index] = contact_info.depth;
+                }
+                else
+                {
+                    it = std::find(g_robot_links.begin(), g_robot_links.end(), link_2);
+                    if (it != g_robot_links.end())
+                    {
+                        int index = it - g_robot_links.begin();
+                        sorted_distances[index] = contact_info.depth;
+                    }
+                }
+                
+            }
 
         }
 
@@ -212,14 +252,19 @@ class DataNode
             g_file << g_next_line.str() << std::endl;
         }
 
-        static void emptyCallback(const std_msgs::String::ConstPtr& msg)
+        std::map<std::string, double> getCurrentStateValues()
+        {
+            return g_planning_scene_monitor->getStateMonitor()->getCurrentStateValues();
+        }
+
+        static void emptyCallback(std_msgs::String msg)
         {
             
         }
 
-        static void contactMapCallback(const morpheus_msgs::ContactMap::ConstPtr& msg)
+        static void contactMapCallback(morpheus_msgs::ContactMap msg)
         {
-            g_latest_contactmap = std::copy(msg);
+            g_latest_contactmap = msg;
             received_contactmap = true;
         }
 
